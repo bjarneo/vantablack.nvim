131d79669d48db298d7f094857b85d26dda8ca51
Bjarne Øverli <bjarne.oeverli@gmail.com>
2026-02-06 19:59:54 +0100

    Initial vantablack

---
+++ colors/vantablack.lua
@@ -0,0 +1,25 @@
+require("aether").load({
+  name = "vantablack",
+  branch = "v2",
+  colors = {
+    -- Background colors
+    bg = "#0d0d0d",
+    bg_dark = "#0d0d0d",
+    bg_highlight = "#fdfdfd",
+
+    -- Foreground colors
+    fg = "#ffffff",
+    fg_dark = "#ececec",
+    comment = "#fdfdfd",
+
+    -- Accent colors
+    red = "#a4a4a4",
+    orange = "#a4a4a4",
+    yellow = "#cecece",
+    green = "#b6b6b6",
+    cyan = "#b0b0b0",
+    blue = "#8d8d8d",
+    purple = "#9b9b9b",
+    magenta = "#9b9b9b",
+  },
+})
